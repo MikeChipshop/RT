74ea3f4d4997294dda7cf1ac24dd090b1b40eb15
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-10-18 20:00:47 +0100

    Footer changes

--- footer.php
+++ footer.php
@@ -5,15 +5,15 @@
     <div class="social-block">
         <h3>Follow us on Facebook, Twitter and Google+</h3>
         <ul>
-            <li><a href="https://plus.google.com/105097231822426699100/about" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/google-plus.png" alt="Find us on Google Plus" /></a></li>
-            <li><a href="https://www.facebook.com/RTAlkinLeadworkandRoofing" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/facebook.png" alt="Find us on Facebook" /></a></li>
-            <li><a href="https://twitter.com/rtalkinleadwork" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/twitter.png" alt="Find us on Twitter" /></a></li>
+            <li><a href="https://plus.google.com/105097231822426699100/about" target="_blank" rel=”noreferrer”><img src="<?php bloginfo('stylesheet_directory'); ?>/images/google-plus.png" alt="Find us on Google Plus" /></a></li>
+            <li><a href="https://www.facebook.com/RTAlkinLeadworkandRoofing" target="_blank" rel=”noreferrer”><img src="<?php bloginfo('stylesheet_directory'); ?>/images/facebook.png" alt="Find us on Facebook" /></a></li>
+            <li><a href="https://twitter.com/rtalkinleadwork" target="_blank" rel=”noreferrer”><img src="<?php bloginfo('stylesheet_directory'); ?>/images/twitter.png" alt="Find us on Twitter" /></a></li>
         </ul>
     </div>
 		<div id="site-generator">
 
 			<?php echo __('&copy; ', 'attorney') . esc_attr( get_bloginfo( 'name', 'display' ) );  ?>
-            &nbsp;|&nbsp;&nbsp;Site by <a href="http://francesca-designed.me">francesca-designed.me</a> | <a href="https://miniman-webdesign.co.uk" rel="noreferrer" target="_blank">Miniman</a>
+            &nbsp;|&nbsp;&nbsp;Site by <a href="http://francesca-designed.me" rel=”noreferrer”>francesca-designed.me</a> | <a href="https://miniman-webdesign.co.uk" rel="noreferrer" target="_blank">Miniman</a>
 
 		</div>
 	</footer><!-- #colophon -->
