19f41faaf286a13f31145d14eff9c1ab6ace2b97
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-10-29 08:21:39 +0000

    Various

--- footer.php
+++ footer.php
@@ -1,13 +1,30 @@
 <footer id="colophon" role="contentinfo">
-    <div class="social-block">
-        <h3>Follow us on Google+, Facebook, Twitter and Houzz</h3>
+    <section class="social-block">
+        <h1>Follow us on Google+, Facebook, Twitter and Houzz</h1>
         <ul>
           <li><a target="_blank" rel="noreferrer" href="https://business.google.com/dashboard/l/00218186411982608153?hl=en" title="Find RT Alkin on Google+"><i class="fab fa-google-plus-square"></i></a></li>
           <li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/RTAlkinLeadworkandRoofing" title="Find RT Alkin on Facebook"><i class="fab fa-facebook-square"></i></a></li>
           <li><a target="_blank" rel="noreferrer" href="https://twitter.com/rtalkinleadwork" title="Follow RT alkin on Twitter"><i class="fab fa-twitter-square"></i></a></li>
           <li><a target="_blank" rel="noreferrer" href="https://www.houzz.co.uk/pro/rtalkinleadwork/rt-alkin-leadwork" title="Find RT Alkin on Houzz"><i class="fab fa-houzz"></i></a></li>
       </ul>
-    </div>
+    </section>
+    <section class="rt_get-in-touch" itemscope itemtype="http://schema.org/LocalBusiness">
+
+        <h1>Get in Touch</h1>
+        <div itemprop="image" itemscope itemtype="http://schema.org/ImageObject">
+            <img src="https://www.rtalkinleadworkandroofing.co.uk/wp-content/themes/RTAlkin/images/rt-alkin-logo.png" width="250" itemprop="url">
+        </div>
+        <ul>
+            <li itemprop="name">RT Alkin</li>
+            <li>Tel: <a href="tel:07703102725"><span itemprop="telephone">07703 102725</span></a></li>
+            <li>Email: <a href="mailto:richard@rtalkin.co.uk" itemprop="email">richard@rtalkin.co.uk</a></li>
+        </ul>
+        <ul itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
+            <li itemprop="streetAddress">St Michaels</li>
+            <li itemprop="addressLocality">Tenterden</li>
+            <li itemprop="postalCode">TN30 6PA</li>
+        </ul>
+    </section>
 
     </footer><!-- #colophon -->
     </div><!-- #container -->
